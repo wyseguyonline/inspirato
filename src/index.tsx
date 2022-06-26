4f4f0f0acccb6f82218537cb77fccfbbefafef1c
Jonathan Wyse <jonathan.wyse@pearson.com>
2022-06-26 16:52:45 -0600

    trimming create-react-app cruft add formatted json file for readability purposes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
